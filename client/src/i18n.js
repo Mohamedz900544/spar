b95cea4b0131c242a4f1aaf0f406d1be50a0124a
Mohamed Zalama <mohamedz90054@gmail.com>
2026-05-08 16:07:11 +0300

    SS

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -3,12 +3,25 @@ import { initReactI18next } from "react-i18next";
 import en from "./locales/en.json";
 import ar from "./locales/ar.json";
 
+const savedLanguage = localStorage.getItem("sparvi_lang");
+const migratedDefaultLanguage = localStorage.getItem("sparvi_lang_default_migrated");
+
+let initialLanguage = savedLanguage || "en";
+
+if (savedLanguage === "ar" && migratedDefaultLanguage !== "true") {
+  initialLanguage = "en";
+  localStorage.setItem("sparvi_lang", "en");
+  localStorage.setItem("sparvi_lang_default_migrated", "true");
+} else if (!migratedDefaultLanguage) {
+  localStorage.setItem("sparvi_lang_default_migrated", "true");
+}
+
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
     ar: { translation: ar },
   },
-  lng: localStorage.getItem("sparvi_lang") || "ar",
+  lng: initialLanguage,
   fallbackLng: "en",
   interpolation: { escapeValue: false },
 });
